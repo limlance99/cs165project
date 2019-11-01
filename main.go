5d77b95a7d9e40fba5f37a12f00662e5690bbcbc
limlance99 <limlance_99@yahoo.com>
2019-11-02 03:12:50 +0800

    fixed stuff

--- main.go
+++ main.go
@@ -24,20 +24,20 @@ func main() {
 
 	api := router.Group("/api")
 	{
-		api.GET("/restrictions", database.GetRestrictions)
-		api.GET("/conditions", database.GetConditions)
-		api.GET("/resandconds", database.GetResAndConds)
-		api.GET("/bodytype", database.GetBodyType)
-		api.GET("/personaldetails", database.GetPersonalDetails)
-		api.GET("/familyrelations", database.GetFamilyRelations)
-		api.GET("/nationality", database.GetNationality)
-		api.GET("/personinformation", database.GetPersonInformation)
-		api.GET("/businesses", database.GetBusinesses)
-		api.GET("/civilstatus", database.GetCivilStatus)
-		api.GET("/dlaf/:id", database.GetOneDLAF)
-		api.GET("/dlaf", database.GetDLAF)
-		api.GET("/", func(c echo.Context) error {
-			return c.String(200, "Hello World")
+		api.GET("/restrictions/", database.GetRestrictions)
+		api.GET("/conditions/", database.GetConditions)
+		api.GET("/resandconds/", database.GetResAndConds)
+		api.GET("/bodytype/", database.GetBodyType)
+		api.GET("/personaldetails/", database.GetPersonalDetails)
+		api.GET("/familyrelations/", database.GetFamilyRelations)
+		api.GET("/nationality/", database.GetNationality)
+		api.GET("/personinformation/", database.GetPersonInformation)
+		api.GET("/businesses/", database.GetBusinesses)
+		api.GET("/civilstatus/", database.GetCivilStatus)
+		api.GET("/dlaf/:id/", database.GetOneDLAF)
+		api.GET("/dlaf/", database.GetDLAF)
+		api.GET("/easteregg", func(c echo.Context) error {
+			return c.String(200, "Save the turtles!")
 		})
 	}
 
